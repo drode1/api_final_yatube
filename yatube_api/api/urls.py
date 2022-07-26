86cdead96c658ddf9cbc8823fdc025ca622740f0
Egor Remezov <mr.drode@gmail.com>
2022-07-27 02:58:49 +0300

    Убрал raw параметр в урлах

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,16 +6,16 @@ from rest_framework.routers import DefaultRouter
 from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register(r'v1/posts', PostViewSet, basename='posts')
-router_v1.register(r'v1/groups', GroupViewSet, basename='groups')
+router_v1.register('v1/posts', PostViewSet, basename='posts')
+router_v1.register('v1/groups', GroupViewSet, basename='groups')
 router_v1.register(r'v1/posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
-router_v1.register(r'v1/follow', FollowViewSet, basename='follows')
+router_v1.register('v1/follow', FollowViewSet, basename='follows')
 
 urlpatterns = [
-    url(r'v1/', include('djoser.urls')),
-    url(r'v1/', include('djoser.urls.jwt')),
-    path(r'v1/redoc/', TemplateView.as_view(template_name='redoc.html'),
+    url('v1/', include('djoser.urls')),
+    url('v1/', include('djoser.urls.jwt')),
+    path('v1/redoc/', TemplateView.as_view(template_name='redoc.html'),
          name='redoc'
          ),
 ]
